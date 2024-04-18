8381ee324d46db8a9f2e5a154b0c992a53b6d826
Bastien <bastiendurand84@gmail.com>
2024-04-18 22:23:14 +0200

    Début vrai map + création vie
    oui ca marche pas la vie

---
+++ Assets/Scripts/Enemy/EnnemyFollowScript.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemyFollowScript : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] float agroRange;
+    [SerializeField] float moveSpeed;
+
+    Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //distance to player
+        float distToPlayer = Vector2.Distance(transform.position, player.transform.position);
+        
+        if (distToPlayer < agroRange) 
+        {
+            //code to chase
+            ChasePlayer();
+        }
+        else 
+        {
+            //stop chasing player
+            StopChasePlayer();
+        }
+    }
+
+    void ChasePlayer()
+    {
+        if (transform.position.x < player.transform.position.x) 
+        {
+            //enemy is to the left side of the player, so move right
+            rb2d.velocity = new Vector2(moveSpeed, 0);
+        }
+        else
+        {
+            //enemy is to the right side of the player, so move left
+            rb2d.velocity = new Vector2 (-moveSpeed, 0);
+        }
+    }
+
+    void StopChasePlayer()
+    {
+        rb2d.velocity = new Vector2(0, 0);
+    }
+
+    //if (collision.tag == "Player") 
+    //{
+    //    var HealthComponent = collision.GetComponent<Health>();
+    //    if (HealthComponent != null) 
+    //    {
+    //        HealthComponent.TakeDamaged(1);
+    //    }
+    //}
+}
