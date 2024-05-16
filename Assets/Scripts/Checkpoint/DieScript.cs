31b093182c9c91f32b104ea9e4d26d955101a45d
Bastiminouuu <bastiendurand84@gmail.com>
2024-05-16 09:03:27 +0200

    Correction bug cam + correction EnemyShoot Error
    ENFINNNNNN

--- Assets/Scripts/Checkpoint/DieScript.cs
+++ Assets/Scripts/Checkpoint/DieScript.cs
@@ -3,14 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class DieScript : MonoBehaviour
 {
     public Health healthscript;
     public GameObject player;
     public GameObject RespawnPoint;
     public bool EnVie = true;
-
+    [SerializeField] CinemachineVirtualCamera cam1;     //set camera pos1
+    [SerializeField] CinemachineVirtualCamera camEnemy;     //set camera pos2
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -18,6 +18,8 @@ public class DieScript : MonoBehaviour
         {
             Debug.Log("Die chute");
             healthscript.TakeDamaged(1);
+            cam1.Priority = 100;
+            camEnemy.Priority = 1;
             player.transform.position = RespawnPoint.transform.position;
         }
     }
